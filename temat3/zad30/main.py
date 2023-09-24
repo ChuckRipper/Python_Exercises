7e9475df77226875afd82f4a15bd86f5a1532a11
Cezary Kalinowski <ckalinowski123@gmail.com>
2023-09-24 22:56:10 +0000

    Update 2 files
    - /temat3/zad30/main.py
    - /temat3/zad31/main.py

--- temat3/zad30/main.py
+++ temat3/zad30/main.py
@@ -1,3 +1,11 @@
-unikalne_wartosci = set(val for tuple_vals in wejscie.values() for val in tuple_vals)
+# Wariant #1
+# unikalne_wartosci = set(val for tuple_vals in wejscie.values() for val in tuple_vals)
+# for wartosc in unikalne_wartosci:
+#     print(wartosc)
+
+# Wariant #2
+unikalne_wartosci = set()
+for tuple_vals in wejscie.values():
+    unikalne_wartosci.update(tuple_vals)
 for wartosc in unikalne_wartosci:
     print(wartosc)
--- temat3/zad31/main.py
+++ temat3/zad31/main.py
@@ -1,3 +1,11 @@
-unikalne_wartosci = set(val for list_vals in wejscie.values() for val in list_vals)
+# Wariant #1
+# unikalne_wartosci = set(val for list_vals in wejscie.values() for val in list_vals)
+# for wartosc in unikalne_wartosci:
+#     print(wartosc)
+
+# Wariant #2
+unikalne_wartosci = set()
+for list_vals in wejscie.values():
+    unikalne_wartosci.update(list_vals)
 for wartosc in unikalne_wartosci:
     print(wartosc)
